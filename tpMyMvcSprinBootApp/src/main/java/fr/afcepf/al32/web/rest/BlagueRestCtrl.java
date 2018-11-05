6f3d3304aaa03ca88d64afca8324aef3c1c18b0b
= <Alexis_Masson_@hotmail.fr>
2018-11-05 11:27:51 +0100

    maj du repo avec la partie ajax

--- tpMyMvcSprinBootApp/src/main/java/fr/afcepf/al32/web/rest/BlagueRestCtrl.java
+++ tpMyMvcSprinBootApp/src/main/java/fr/afcepf/al32/web/rest/BlagueRestCtrl.java
@@ -6,7 +6,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +31,7 @@ public class BlagueRestCtrl {
 	
 	// constructeur vide
 	public BlagueRestCtrl() {
-		mapBlagues.put(1L, new Blague(1L, "blague sur les juifs",
+		mapBlagues.put(1L, new Blague(1L, "Gueubla sur les juifs",
 				"Mr est madame Juif ? --> Gaston", 3.4));
 		mapBlagues.put(2L, new Blague(2L, "blague sur les noirs",
 				"Jean Negro sur la patate", 4.0));
@@ -37,9 +40,14 @@ public class BlagueRestCtrl {
 		mapBlagues.put(4L, new Blague(4L, "blague nulle pas très drôle. pas lol.",
 				"Pas lol du tout.", 1.0));
 		mapBlagues.put(5L, new Blague(5L, "blague drôle sur Zyzman.",
-				"Cré cré laul", 4.8));
+				"Cré cré laul", 2.0));
+		mapBlagues.put(6L, new Blague(6L, "Les filles",
+				"Pourquoi les filles se maquillent et mettent du parfum ? -> parce qu'elles sont moches et qu'elles puent.", 4.4));
+		mapBlagues.put(7L, new Blague(7L, "Ou trouve t-on un tétraplégique ?",
+				"Ou trouve t-on un tétraplégique -> Là où on l'a laissé.", 1.8));
 	}
 
+	
 	// URL = http://localhost:8080/tpMvcSpringBootApp/rest/blagues
 	// avec {"titre" : "nouvelleBlague", "texte" : "blague ..." }
 	// dans la partie body invisible de la requête HTTP
@@ -66,5 +74,16 @@ public class BlagueRestCtrl {
 					.collect(Collectors.toList());
 		}
 		
+	}
+	
+	//URL = http://localhost:8080/tpMvcSpringBootApp/rest/blagues/1
+	@GetMapping("/{id}")
+	public ResponseEntity<Blague> getBlagueById(@PathVariable("id") Long id) {
+		Blague b = mapBlagues.get(id);
+		if(b==null) {
+			return new ResponseEntity<Blague>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<Blague>(b, HttpStatus.OK);
+		}
 	}
 }
